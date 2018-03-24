b704ff7a4869d078133c67b6689d7904d7bd7af9
omri nitecki <fwgwie@gmail.com>
2018-03-24 22:05:57 +0300

    heroku 2.0

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,22 +4,20 @@ import javax.servlet.http.*;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
 
-public class Cloudio extends HttpServlet
- {
+public class HelloWorld extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-	{
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         resp.getWriter().print("Hello from Java!\n");
     }
 
-    public static void main(String[] args) throws Exception
-	{
-        Server server = new CloudioServer(Integer.valueOf(System.getenv("PORT")));
+    public static void main(String[] args) throws Exception{
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         server.setHandler(context);
-        context.addServlet(new ServletHolder(new Cloudio()),"/*");
+        context.addServlet(new ServletHolder(new HelloWorld()),"/*");
         server.start();
         server.join();   
     }
